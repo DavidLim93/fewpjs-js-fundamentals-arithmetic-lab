140887b58bcf9e784a9b6c3b8195a145eb495cc2
DavidLim93 <lim.david93@gmail.com>
2022-07-26 19:42:13 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+
+let num3=32
+let num4=8
+const mod = num3/num4
+
+const max = Math.max(4, 13, 20, 18, 9)
